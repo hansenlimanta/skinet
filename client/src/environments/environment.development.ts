4f91d2fbc10a2480b4895a396a341eb41b669f32
Hansen Limanta <hansenlimanta@gmail.com>
2025-02-18 22:32:09 +0700

    feat: add checkout functionality from BE and FE;

--- client/src/environments/environment.development.ts
+++ client/src/environments/environment.development.ts
@@ -1,4 +1,6 @@
 export const environment = {
   production: false,
   apiUrl: 'https://localhost:5001/api/',
+  stripePublicKey:
+    'pk_test_51QpWfpE97uYh0sWbnmYIqX5vrA6D7NyJCTjgIkCY1o3eyiAtzBsaRi8JGgqdUVG1GuqlNhaSgddhnZ6M975suJ5k00Yl1ZzV60',
 };
--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -1,4 +1,6 @@
 export const environment = {
   production: true,
   apiUrl: 'api/',
+  stripePublicKey:
+    'pk_test_51QpWfpE97uYh0sWbnmYIqX5vrA6D7NyJCTjgIkCY1o3eyiAtzBsaRi8JGgqdUVG1GuqlNhaSgddhnZ6M975suJ5k00Yl1ZzV60',
 };
